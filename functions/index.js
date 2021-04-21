d1f992246a07531498f0637e5faea720dcd88c6e
Dorci <280145@gmail.com>
2021-04-21 14:31:36 +0200

    [initial] setup

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
 const functions = require("firebase-functions");
-
+const firebaseAdmin = require("firebase-admin");
+const glob = require("glob");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+firebaseAdmin.initializeApp();
+
+exports.testFunction = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+const files = glob.sync("./**/*.f.js", {
+  cwd: __dirname,
+  ignore: "./node_modules/**",
+});
+
+for (const file of files) {
+  const functionName = file.split("/").pop().slice(0, -5); // Strip off '.f.js'
+  const functionTarget =
+    process.env.FUNCTION_TARGET || process.env.FUNCTION_NAME || "";
+
+  // required for multi-function exports, e.g. futureArrivals.cronjob
+  const rootFunction = functionTarget.split(".")[0];
+
+  if (!rootFunction || rootFunction === functionName) {
+    exports[functionName] = require(file);
+  }
+}
