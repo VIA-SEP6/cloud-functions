b72288354b60409eff4300f29509810439557b65
Dorci <280145@gmail.com>
2021-05-12 18:27:31 +0200

    [favouriteMovies] add and remove favourite movies for a specific user

--- functions/user/user.f.js
+++ functions/user/user.f.js
@@ -1,6 +1,11 @@
 const functions = require("firebase-functions");
 const { info, error } = require("../util/logger");
 const { db, admin } = require("../util/adminDbUtil");
+const {
+  addFavourite,
+  removeFavourite,
+} = require("../util/reactions/reactionService");
+
 module.exports = {
   register: functions.region("europe-west1").https.onCall(async (data) => {
     const { userName, password, email, userInfo } = data;
@@ -68,7 +73,7 @@ module.exports = {
       const docRef = db.collection("users").doc(`${userId}`);
       const userDoc = await docRef.get();
       if (!userDoc.exists)
-        return { status: 400, message: "User does not exist" };
+        throw new HttpsError("aborted", "User does not exist.");
       return userDoc.favouriteMovies;
     }),
 
@@ -79,14 +84,24 @@ module.exports = {
       const docRef = db.collection("users").doc(`${userId}`);
       const userDoc = await docRef.get();
       if (!userDoc.exists)
-        return { status: 400, message: "User does not exist" };
-      return db
-        .collection("users")
-        .doc(`${user.uid}`)
-        .set({ favouriteMovies }, { merge: true })
-        .then(() => {
-          info(`Register User | Successful | ${user.uid}`);
-          return { status: 200, message: { user: user.uid } };
-        });
+        throw new HttpsError("aborted", "User does not exist.");
+      addFavourite("users", movieId, userId).then(() => {
+        info(`addFavouriteMovie | Successful | ${userId} | ${movieId}`);
+        return { status: 200, message: { addFavouriteMovie: "successful" } };
+      });
+    }),
+
+  removeFavouriteMovie: functions
+    .region("europe-west1")
+    .https.onCall(async (data) => {
+      const { userId, movieId } = data;
+      const docRef = db.collection("users").doc(`${userId}`);
+      const userDoc = await docRef.get();
+      if (!userDoc.exists)
+        throw new HttpsError("aborted", "User does not exist.");
+      removeFavourite("users", movieId, userId).then(() => {
+        info(`removeFavouriteMovie | Successful | ${userId} | ${movieId}`);
+        return { status: 200, message: { removeFavouriteMovie: "successful" } };
+      });
     }),
 };
--- functions/util/reactions/reactionService.js
+++ functions/util/reactions/reactionService.js
@@ -1,99 +1,132 @@
-const {db} = require("../adminDbUtil");
+const { db } = require("../adminDbUtil");
+const { info } = require("../../util/logger");
 
 const likeTopic = async (collection, topicId, userUid) => {
-	const topicReference = db.collection(collection).doc(topicId);
+  const topicReference = db.collection(collection).doc(topicId);
 
-	return new Promise(async (resolve, reject) => {
-		const topicDocument = await topicReference.get();
-		if (!topicDocument.exists) {
-			reject({code: "404", error: "Document not found"});
-			return;
-		}
+  return new Promise(async (resolve, reject) => {
+    const topicDocument = await topicReference.get();
+    if (!topicDocument.exists) {
+      reject({ code: "404", error: "Document not found" });
+      return;
+    }
 
-		let {likes} = topicDocument.data();
-		const {dislikes} = topicDocument.data();
+    let { likes } = topicDocument.data();
+    const { dislikes } = topicDocument.data();
 
-		if (dislikes && dislikes.includes(userUid)) {
-			const filteredDislikes = dislikes.filter((id) => id !== userUid);
-			topicReference.set({dislikes: filteredDislikes}, {merge: true});
-		}
+    if (dislikes && dislikes.includes(userUid)) {
+      const filteredDislikes = dislikes.filter((id) => id !== userUid);
+      topicReference.set({ dislikes: filteredDislikes }, { merge: true });
+    }
 
-		if (likes && likes.includes(userUid)) {
-			return;
-		}
+    if (likes && likes.includes(userUid)) {
+      return;
+    }
 
-		if (!likes) {
-			likes = [];
-		}
+    if (!likes) {
+      likes = [];
+    }
 
-		likes.push(userUid);
-		topicReference.set({likes: likes}, {merge: true});
-		resolve("success");
-	});
+    likes.push(userUid);
+    topicReference.set({ likes: likes }, { merge: true });
+    resolve("success");
+  });
 };
 
 const dislikeTopic = async (collection, topicId, userUid) => {
-	const topicReference = db.collection(collection).doc(topicId);
+  const topicReference = db.collection(collection).doc(topicId);
 
-	return new Promise(async (resolve, reject) => {
-		const topicDocument = await topicReference.get();
-		if (!topicDocument.exists) {
-			reject({code: "404", error: "Document not found"});
-			return;
-		}
+  return new Promise(async (resolve, reject) => {
+    const topicDocument = await topicReference.get();
+    if (!topicDocument.exists) {
+      reject({ code: "404", error: "Document not found" });
+      return;
+    }
 
-		const {likes} = topicDocument.data();
-		let {dislikes} = topicDocument.data();
+    const { likes } = topicDocument.data();
+    let { dislikes } = topicDocument.data();
 
-		if (likes && likes.includes(userUid)) {
-			const filteredLikes = likes.filter((id) => id !== userUid);
-			topicReference.set({likes: filteredLikes}, {merge: true});
-		}
+    if (likes && likes.includes(userUid)) {
+      const filteredLikes = likes.filter((id) => id !== userUid);
+      topicReference.set({ likes: filteredLikes }, { merge: true });
+    }
 
-		if (dislikes && dislikes.includes(userUid)) {
-			resolve("success");
-			return;
-		}
+    if (dislikes && dislikes.includes(userUid)) {
+      resolve("success");
+      return;
+    }
 
-		if (!dislikes) {
-			dislikes = [];
-		}
+    if (!dislikes) {
+      dislikes = [];
+    }
 
-		dislikes.push(userUid);
-		topicReference.set({dislikes: dislikes}, {merge: true});
-		resolve("success");
-	});
+    dislikes.push(userUid);
+    topicReference.set({ dislikes: dislikes }, { merge: true });
+    resolve("success");
+  });
 };
 
 const clearReactionFromTopic = async (collection, topicId, userUid) => {
-	const topicReference = db.collection(collection).doc(topicId);
+  const topicReference = db.collection(collection).doc(topicId);
 
-	return new Promise(async (resolve, reject) => {
-		const topicDocument = await topicReference.get();
-		if (!topicDocument.exists) {
-			reject({code: "404", error: "Document not found"});
-			return;
-		}
+  return new Promise(async (resolve, reject) => {
+    const topicDocument = await topicReference.get();
+    if (!topicDocument.exists) {
+      reject({ code: "404", error: "Document not found" });
+      return;
+    }
 
-		const {likes} = topicDocument.data();
-		const {dislikes} = topicDocument.data();
+    const { likes } = topicDocument.data();
+    const { dislikes } = topicDocument.data();
 
-		if (likes && likes.includes(userUid)) {
-			const filteredLikes = likes.filter((id) => id !== userUid);
-			topicReference.set({likes: filteredLikes}, {merge: true});
-		}
+    if (likes && likes.includes(userUid)) {
+      const filteredLikes = likes.filter((id) => id !== userUid);
+      topicReference.set({ likes: filteredLikes }, { merge: true });
+    }
 
-		if (dislikes && dislikes.includes(userUid)) {
-			const filteredDislikes = dislikes.filter((id) => id !== userUid);
-			topicReference.set({dislikes: filteredDislikes}, {merge: true});
-		}
+    if (dislikes && dislikes.includes(userUid)) {
+      const filteredDislikes = dislikes.filter((id) => id !== userUid);
+      topicReference.set({ dislikes: filteredDislikes }, { merge: true });
+    }
 
-		resolve("success");
-	});
+    resolve("success");
+  });
+};
+const addFavourite = async (collection, movieId, userUid) => {
+  const collecReference = db.collection(collection).doc(userUid);
+  const doc = await collecReference.get();
+  let { favouriteMovies } = doc.data();
+  if (!favouriteMovies) favouriteMovies = [];
+  favouriteMovies.push(movieId);
+  info(favouriteMovies);
+  return collecReference.set(
+    {
+      favouriteMovies: [...new Set(favouriteMovies)],
+    },
+    {
+      merge: true,
+    }
+  );
+};
+const removeFavourite = async (collection, movieId, userUid) => {
+  const collecReference = db.collection(collection).doc(userUid);
+  const doc = await collecReference.get();
+  let { favouriteMovies } = doc.data();
+  if (favouriteMovies && favouriteMovies.includes(movieId)) {
+    const filterFavMovies = favouriteMovies.filter((id) => id !== movieId);
+    return collecReference.set(
+      {
+        favouriteMovies: filterFavMovies,
+      },
+      { merge: true }
+    );
+  }
 };
 
 module.exports = {
-	likeTopic,
-	dislikeTopic,
-	clearReactionFromTopic
+  likeTopic,
+  dislikeTopic,
+  clearReactionFromTopic,
+  addFavourite,
+  removeFavourite,
 };
