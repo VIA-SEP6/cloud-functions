4e7cd260f0c76cb6085928255824e3f9f9546e1e
David Franko <david.franko1998@gmail.com>
2021-05-18 22:26:42 +0200

    Added favourite movies to get user profile request

--- functions/user/user.f.js
+++ functions/user/user.f.js
@@ -1,108 +1,119 @@
 const functions = require("firebase-functions");
+const {HttpsError} = require("firebase-functions/lib/providers/https");
+const {getMovieRequest} = require("../movies/services/movieAPIService");
 const {info, error} = require("../util/logger");
 const {db, admin} = require("../util/adminDbUtil");
 const {
-	addFavourite,
-	removeFavourite
+    addFavourite,
+    removeFavourite
 } = require("../util/reactions/reactionService");
 
 module.exports = {
-	register: functions.region("europe-west1").https.onCall(async (data) => {
-		const {userName, password, email, userInfo} = data;
-		if (!userName || !password || !email)
-			throw new HttpsError("failed-precondition", "Missing required data.");
+    register: functions.region("europe-west1").https.onCall(async (data) => {
+        const {userName, password, email, userInfo} = data;
+        if (!userName || !password || !email)
+            throw new HttpsError("failed-precondition", "Missing required data.");
 
-		return admin
-			.auth()
-			.createUser({
-				disabled: false,
-				emailVerified: false,
-				email: email,
-				password: password,
-				displayName: userName
-			})
-			.then((user) => {
-				return db
-					.collection("users")
-					.doc(`${user.uid}`)
-					.set({...userInfo, email, userName}, {merge: true})
-					.then(() => {
+        return admin
+            .auth()
+            .createUser({
+                disabled: false,
+                emailVerified: false,
+                email: email,
+                password: password,
+                displayName: userName
+            })
+            .then((user) => {
+                return db
+                    .collection("users")
+                    .doc(`${user.uid}`)
+                    .set({...userInfo, email, userName}, {merge: true})
+                    .then(() => {
 
-						info(`Register User | Successful | ${user.uid}`);
-						return {status: 200, message: {user: user.uid}};
-					});
-			})
-			.catch((err) => {
-				error(`Register User | Error | ${err}`);
-				throw new HttpsError("aborted", "User not registered.");
-			});
-	}),
+                        info(`Register User | Successful | ${user.uid}`);
+                        return {status: 200, message: {user: user.uid}};
+                    });
+            })
+            .catch((err) => {
+                error(`Register User | Error | ${err}`);
+                throw new HttpsError("aborted", "User not registered.");
+            });
+    }),
 
-	getProfile: functions.region("europe-west1").https.onCall(async (data) => {
-		const {userId} = data;
-		const docRef = db.collection("users").doc(`${userId}`);
-		const userDoc = await docRef.get();
-		if (!userDoc.exists)
-			throw new HttpsError("aborted", "User does not exist.");
+    getProfile: functions.region("europe-west1").https.onCall(async (data) => {
+        const {userId} = data;
+        const docRef = db.collection("users").doc(`${userId}`);
+        const userDoc = await docRef.get();
+        if (!userDoc.exists)
+            throw new HttpsError("aborted", "User does not exist.");
 
-		return {status: 200, message: {user: userDoc.data()}};
-	}),
+        const userData = userDoc.data()
 
-	updateProfile: functions.region("europe-west1").https.onCall(async (data) => {
-		const {user, userId} = data;
-		if (!userId)
-			throw new HttpsError("failed-precondition", "Missing required data.");
+        const favouriteMovies = []
+        await Promise.all(userData.favouriteMovies?.map(async movieId => {
+            favouriteMovies.push(await getMovieRequest(movieId))
+        }))
+        userData.favouriteMovies = favouriteMovies
 
-		const docRef = db.collection("users").doc(`${userId}`);
-		const userDoc = await docRef.get();
-		if (!userDoc.exists)
-			throw new HttpsError("aborted", "User does not exist.");
-		return db
-			.collection("users")
-			.doc(`${userId}`)
-			.set(user, {merge: true})
-			.then(() => {
-				info(`Update User | Successful | ${userId}`);
-				return {status: 200, message: {update_sucessful: userId}};
-			});
-	}),
 
-	getFavouriteMovies: functions
-		.region("europe-west1")
-		.https.onCall(async (data) => {
-			const {userId} = data;
-			const docRef = db.collection("users").doc(`${userId}`);
-			const userDoc = await docRef.get();
-			if (!userDoc.exists)
-				throw new HttpsError("aborted", "User does not exist.");
-			return userDoc.favouriteMovies;
-		}),
+        return {status: 200, message: {user: userData}};
+    }),
 
-	addFavouriteMovie: functions
-		.region("europe-west1")
-		.https.onCall(async (data) => {
-			const {userId, movieId} = data;
-			const docRef = db.collection("users").doc(`${userId}`);
-			const userDoc = await docRef.get();
-			if (!userDoc.exists)
-				throw new HttpsError("aborted", "User does not exist.");
-			addFavourite("users", movieId, userId).then(() => {
-				info(`addFavouriteMovie | Successful | ${userId} | ${movieId}`);
-				return {status: 200, message: {addFavouriteMovie: "successful"}};
-			});
-		}),
+    updateProfile: functions.region("europe-west1").https.onCall(async (data) => {
+        const {user, userId} = data;
+        if (!userId)
+            throw new HttpsError("failed-precondition", "Missing required data.");
 
-	removeFavouriteMovie: functions
-		.region("europe-west1")
-		.https.onCall(async (data) => {
-			const {userId, movieId} = data;
-			const docRef = db.collection("users").doc(`${userId}`);
-			const userDoc = await docRef.get();
-			if (!userDoc.exists)
-				throw new HttpsError("aborted", "User does not exist.");
-			removeFavourite("users", movieId, userId).then(() => {
-				info(`removeFavouriteMovie | Successful | ${userId} | ${movieId}`);
-				return {status: 200, message: {removeFavouriteMovie: "successful"}};
-			});
-		})
+        const docRef = db.collection("users").doc(`${userId}`);
+        const userDoc = await docRef.get();
+        if (!userDoc.exists)
+            throw new HttpsError("aborted", "User does not exist.");
+        return db
+            .collection("users")
+            .doc(`${userId}`)
+            .set(user, {merge: true})
+            .then(() => {
+                info(`Update User | Successful | ${userId}`);
+                return {status: 200, message: {update_sucessful: userId}};
+            });
+    }),
+
+    getFavouriteMovies: functions
+        .region("europe-west1")
+        .https.onCall(async (data) => {
+            const {userId} = data;
+            const docRef = db.collection("users").doc(`${userId}`);
+            const userDoc = await docRef.get();
+            if (!userDoc.exists)
+                throw new HttpsError("aborted", "User does not exist.");
+            return userDoc.favouriteMovies;
+        }),
+
+    addFavouriteMovie: functions
+        .region("europe-west1")
+        .https.onCall(async (data) => {
+            const {userId, movieId} = data;
+            const docRef = db.collection("users").doc(`${userId}`);
+            const userDoc = await docRef.get();
+            if (!userDoc.exists)
+                throw new HttpsError("aborted", "User does not exist.");
+            addFavourite("users", movieId, userId).then(() => {
+                info(`addFavouriteMovie | Successful | ${userId} | ${movieId}`);
+                return {status: 200, message: {addFavouriteMovie: "successful"}};
+            });
+        }),
+
+    removeFavouriteMovie: functions
+        .region("europe-west1")
+        .https.onCall(async (data) => {
+            const {userId, movieId} = data;
+            const docRef = db.collection("users").doc(`${userId}`);
+            const userDoc = await docRef.get();
+            if (!userDoc.exists)
+                throw new HttpsError("aborted", "User does not exist.");
+            removeFavourite("users", movieId, userId).then(() => {
+                info(`removeFavouriteMovie | Successful | ${userId} | ${movieId}`);
+                return {status: 200, message: {removeFavouriteMovie: "successful"}};
+            });
+        })
 };
