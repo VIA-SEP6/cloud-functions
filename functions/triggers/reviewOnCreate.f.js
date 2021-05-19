c6e3483d55efa5c817bf68fef0ba05e067a55272
Dorci <280145@gmail.com>
2021-05-19 12:32:33 +0200

    [reviewOnCreate] update review count on user profile when review is created

--- functions/triggers/reviewOnCreate.f.js
+++ functions/triggers/reviewOnCreate.f.js
@@ -1,14 +1,18 @@
 const functions = require("firebase-functions");
-const {updateMovieReviewRating} = require("../movies/services/movieDAService");
+const {
+  updateMovieReviewRating,
+} = require("../movies/services/movieDAService");
+const { updateReviewsCount } = require("../user/services/userProfileUtils");
 
 module.exports = functions
-	.region("europe-west1")
-	.firestore.document("reviews/{reviewId}")
-	.onCreate(async (snap, context) => {
-		const rating = snap.data().rating
-		const data = {
-			movieId: `${snap.data().movieId}`,
-			rating: parseFloat(rating)
-		};
-		await updateMovieReviewRating(data);
-	});
+  .region("europe-west1")
+  .firestore.document("reviews/{reviewId}")
+  .onCreate(async (snap, context) => {
+    const rating = snap.data().rating;
+    const data = {
+      movieId: `${snap.data().movieId}`,
+      rating: parseFloat(rating),
+    };
+    await updateMovieReviewRating(data);
+    await updateReviewsCount(snap.data().userId);
+  });
