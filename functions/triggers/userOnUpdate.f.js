6c5f6781634acacee33e1f74c0f9f65ef185c6a9
Dorci <280145@gmail.com>
2021-05-19 12:34:28 +0200

    [userOnUpdate] refactor code and extract logic into functions

--- functions/triggers/userOnUpdate.f.js
+++ functions/triggers/userOnUpdate.f.js
@@ -1,70 +1,53 @@
 const functions = require("firebase-functions");
-const {info, error} = require("../util/logger");
-const {db, admin} = require("../util/adminDbUtil");
+const { info, error } = require("../util/logger");
+const { db, admin } = require("../util/adminDbUtil");
 
 module.exports = functions
-	.region("europe-west1")
-	.firestore.document("users/{userId}")
-	.onUpdate((change, context) => {
-		const updatedUser = change.after.data();
-		const {userId} = context.params;
+  .region("europe-west1")
+  .firestore.document("users/{userId}")
+  .onUpdate(async (change, context) => {
+    const updatedUser = change.after.data();
+    const { userId } = context.params;
 
-		db.collection("reviews")
-			.where("userId", "==", userId)
-			.get()
-			.then((snap) => {
-				for (const doc of snap) {
-					updateReviews(doc.id, updatedUser);
-				}
+    await updateReview(userId, updatedUser);
 
-				info(`User update | Successful | ${userId}`);
-				return {status: 200, message: {user: userId}};
-			})
-			.catch((err) => {
-				error(`User update | Error | ${err}`);
-				return {status: 500, message: "User not updated"};
-			});
-		db.collection("comments")
-			.where("userId", "==", userId)
-			.get()
-			.then((snap) => {
-				for (const doc of snap) {
-					updateComments(doc.id, updatedUser);
+    await updateComment(userId, updatedUser);
+  });
+async function updateReview(userId, user) {
+  const review = await db
+    .collection("reviews")
+    .where("userId", "==", userId)
+    .get();
+  if (!review.exists) return;
 
-					info(`User update | Successful | ${userId}`);
-					({status: 200, message: {user: userId}});
-					continue;
-				}
-			})
-			.catch((err) => {
-				error(`User update | Error | ${err}`);
-				return {status: 500, message: "User not updated"};
-			});
-	});
-function updateReviews(reviewId, user) {
-	return db
-		.collection("reviews")
-		.doc(reviewId)
-		.update({user})
-		.catch((err) => {
-			error(`updateReviews | Error | ${err}`);
-			return {
-				status: 500,
-				message: "Reviews not updated with latest user updates"
-			};
-		});
+  return db
+    .collection("reviews")
+    .doc(`${review.id}`)
+    .update({ user })
+    .catch((err) => {
+      error(`updateReviews | Error | ${err}`);
+      return {
+        status: 500,
+        message: "Reviews not updated with latest user updates",
+      };
+    });
 }
 
-function updateComments(commentId, user) {
-	return db
-		.collection("comments")
-		.doc(commentId)
-		.update({user})
-		.catch((err) => {
-			error(`updateComments | Error | ${err}`);
-			return {
-				status: 500,
-				message: "Comments not updated with latest user updates"
-			};
-		});
+async function updateComment(userId, user) {
+  const comment = await db
+    .collection("comments")
+    .where("userId", "==", userId)
+    .get();
+  if (!comment.exists) return;
+  return db
+    .collection("comments")
+    .doc(`${comment.id}`)
+    .update({ user })
+    .catch((err) => {
+      error(`updateComments | Error | ${err}`);
+      return {
+        status: 500,
+        message: "Comments not updated with latest user updates",
+      };
+    });
 }
