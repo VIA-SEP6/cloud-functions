75f3965ce6d634c7876d5d4181f454f3b47a534c
Dorci <280145@gmail.com>
2021-05-19 15:39:15 +0200

    [movies] change onRequest to https.onCallable

--- functions/movies/movies.f.js
+++ functions/movies/movies.f.js
@@ -1,98 +1,72 @@
 const functions = require("firebase-functions");
-const cors = require("cors")({origin: true});
+const { HttpsError } = require("firebase-functions/lib/providers/https");
 const {
 	getMovieRequest,
 	getMoviesRequest,
 	getTopRatedMoviesRequest
 } = require("./services/movieAPIService");
+const { warn } = require("../util/logger");
 const {searchMovieGetRequest} = require("./services/searchAPIService");
 
 module.exports = {
-	get: functions
-		.region("europe-west1")
-		.https.onRequest(async (request, response) => {
-			const {id, append_to_response} = request.body;
-			if (!id)
-				return cors(request, response, () => {
-					response.status(400).send({data: "Missing id"});
-				});
-			await getMovieRequest(id, append_to_response)
-				.then((result) => {
-					return cors(request, response, () => {
-						response.status(200).send({data: result});
-					});
-				})
-				.catch((err) => {
-					return cors(request, response, () => {
-						response.sendStatus(500);
-					});
-				});
-		}),
-	getPopular: functions
-		.region("europe-west1")
-		.https.onRequest(async (request, response) => {
-			await getMoviesRequest('popular')
-				.then((result) => {
-					return cors(request, response, () => {
-						response.status(200).send({data: result});
-					});
-				})
-				.catch((err) => {
-					return cors(request, response, () => {
-						response.sendStatus(500);
-					});
-				});
-		}),
-	getUpcoming: functions
-		.region("europe-west1")
-		.https.onRequest(async (request, response) => {
-			await getMoviesRequest('upcoming')
-				.then((result) => {
-					return cors(request, response, () => {
-						response.status(200).send({data: result});
-					});
-				})
-				.catch((err) => {
-					return cors(request, response, () => {
-						response.sendStatus(500);
-					});
-				});
-		}),
-	getTopRated: functions
-		.region("europe-west1")
-		.https.onRequest(async (request, response) => {
-			await getTopRatedMoviesRequest()
-				.then((result) => {
-					return cors(request, response, () => {
-						response.status(200).send({data: result});
-					});
-				})
-				.catch((err) => {
-					console.log(err);
-					return cors(request, response, () => {
-						response.sendStatus(500);
-					});
-				});
-		}),
-	search: functions
-		.region("europe-west1")
-		.https.onRequest(async (request, response) => {
-			const {query} = request.body;
-			if (!query)
-				return cors(request, response, () => {
-					response.status(400).send({data: "Missing query"});
-				});
-			await searchMovieGetRequest(query)
-				.then((result) => {
-					return cors(request, response, () => {
-						response.status(200).send({data: result});
-					});
-				})
-				.catch((err) => {
-					console.log(err);
-					return cors(request, response, () => {
-						response.status(500).send({data: "Server error " + err});
-					});
-				});
-		})
+  get: functions.region("europe-west1").https.onCall(async (data, context) => {
+    const { id, append_to_response } = data;
+    if (!id)
+      return cors(request, response, () => {
+        response.status(400).send({ data: "Missing id" });
+      });
+    await getMovieRequest(id, append_to_response)
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        new HttpsError("failed-precondition", err);
+      });
+  }),
+  getPopular: functions
+    .region("europe-west1")
+    .https.onCall(async (data, context) => {
+      await getMoviesRequest("popular")
+        .then((result) => {
+          return result;
+        })
+        .catch((err) => {
+          new HttpsError("failed-precondition", err);
+        });
+    }),
+  getUpcoming: functions
+    .region("europe-west1")
+    .https.onCall(async (data, context) => {
+      await getMoviesRequest("upcoming")
+        .then((result) => {
+          return result;
+        })
+        .catch((err) => {
+          new HttpsError("failed-precondition", err);
+        });
+    }),
+  getTopRated: functions
+    .region("europe-west1")
+    .https.onCall(async (data, context) => {
+      await getTopRatedMoviesRequest()
+        .then((result) => {
+          return result;
+        })
+        .catch((err) => {
+          new HttpsError("failed-precondition", err);
+        });
+    }),
+  search: functions
+    .region("europe-west1")
+    .https.onCall(async (data, context) => {
+      const { query } = data;
+      if (!query) new HttpsError("invalid-argument", "Missing required fields");
+      await searchMovieGetRequest(query)
+        .then((result) => {
+          return result;
+        })
+        .catch((err) => {
+          new HttpsError("failed-precondition", err);
+        });
+    }),
 };
