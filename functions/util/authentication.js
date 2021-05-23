e96efb13605fb5059d23af7211ad7aafe9778ba4
David Franko <david.franko1998@gmail.com>
2021-05-24 00:58:09 +0200

    Improved authenticateAndGetUserIdFromContext

--- functions/util/authentication.js
+++ functions/util/authentication.js
@@ -1,6 +1,9 @@
 const {HttpsError} = require("firebase-functions/lib/providers/https");
 
 function authenticateAndGetUserIdFromContext(context) {
+    if (!context || !context.auth) {
+        throw new HttpsError("permission-denied", "Authentication required")
+    }
     const {uid} = context.auth
     if (!uid)
         throw new HttpsError("permission-denied", "Authentication required")
