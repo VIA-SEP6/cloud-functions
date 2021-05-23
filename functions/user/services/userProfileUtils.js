16f5589fc43089b3e112c4aa4675f14f6b2841fc
David Franko <david.franko1998@gmail.com>
2021-05-23 14:16:53 +0200

    Fixed reviews on comments count

--- functions/user/services/userProfileUtils.js
+++ functions/user/services/userProfileUtils.js
@@ -5,7 +5,7 @@ const updateReviewsCount = async (userId) => {
     await db.runTransaction(async (transaction) => {
         const user = await transaction.get(userRef);
         if (!user.exists) return;
-        transaction.update(userRef, {reviews: user.get("nrOfReviews") || 0 + 1});
+        transaction.update(userRef, {nrOfReviews: (user.get("nrOfReviews") || 0) + 1});
     });
 };
 
@@ -14,7 +14,7 @@ const updateCommentsCount = async (userId) => {
     await db.runTransaction(async (transaction) => {
         const user = await transaction.get(userRef);
         if (!user.exists) return;
-        transaction.update(userRef, {comments: user.get("nrOfComments") || 0 + 1});
+        transaction.update(userRef, {nrOfComments: (user.get("nrOfComments") || 0) + 1});
     });
 }
 
