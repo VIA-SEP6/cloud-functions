ad367dd24969a3812a6ea3050c6247a9068f4aac
David Franko <david.franko1998@gmail.com>
2021-05-23 20:15:03 +0200

    Fixed errors and improved efficiency of userUpdate

--- functions/user/triggers/userOnUpdate.f.js
+++ functions/user/triggers/userOnUpdate.f.js
@@ -9,45 +9,37 @@ module.exports = functions
     const updatedUser = change.after.data();
     const { userId } = context.params;
 
-    await updateReview(userId, updatedUser);
+    await updateReviews(userId, updatedUser);
 
-    await updateComment(userId, updatedUser);
+    await updateComments(userId, updatedUser);
   });
-async function updateReview(userId, user) {
-  const review = await db
+
+async function updateReviews(userId, user) {
+  const reviewsSnapshot = await db
     .collection("reviews")
     .where("userId", "==", userId)
     .get();
-  if (!review.exists) return;
 
-  return db
-    .collection("reviews")
-    .doc(`${review.id}`)
-    .update({ user })
-    .catch((err) => {
-      error(`updateReviews | Error | ${err}`);
-      return {
-        status: 500,
-        message: "Reviews not updated with latest user updates",
-      };
-    });
+  const batch = db.batch()
+    reviewsSnapshot.forEach(reviewDoc => {
+        if (reviewDoc.exists) {
+            batch.update(reviewDoc.ref, { user })
+        }
+    })
+    return batch.commit()
 }
 
-async function updateComment(userId, user) {
-  const comment = await db
+async function updateComments(userId, user) {
+  const commentsSnapshot = await db
     .collection("comments")
     .where("userId", "==", userId)
     .get();
-  if (!comment.exists) return;
-  return db
-    .collection("comments")
-    .doc(`${comment.id}`)
-    .update({ user })
-    .catch((err) => {
-      error(`updateComments | Error | ${err}`);
-      return {
-        status: 500,
-        message: "Comments not updated with latest user updates",
-      };
-    });
+
+    const batch = db.batch()
+    commentsSnapshot.forEach(commentDoc => {
+        if (commentDoc.exists) {
+            batch.update(commentDoc.ref, { user })
+        }
+    })
+    return batch.commit()
 }
