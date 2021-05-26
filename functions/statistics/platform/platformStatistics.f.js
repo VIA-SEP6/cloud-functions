3189787cf7da17871f822e435db3c9c911a42258
Dorci <280145@gmail.com>
2021-05-26 17:38:07 +0200

    [platform statistics] add pub/sub scheduler for platform statistics

--- functions/statistics/platform/platformStatistics.f.js
+++ functions/statistics/platform/platformStatistics.f.js
@@ -7,7 +7,7 @@ module.exports = {
   update: functions
     .runWith({ timeoutSeconds: 300, memory: "2GB" })
     .region("europe-west1")
-    .pubsub.schedule("0 0 * * *")
+    .pubsub.schedule("50 23 * * *")
     .onRun(async () => {
       await calculateForPlatformCollection("comments");
       await calculateForPlatformCollection("reviews");
